4fef61c4a06bc7507189454b5edce570515d27f9
tobontob <tobontob@naver.com>
2025-06-10 00:29:48 +0900

    up

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -1,11 +1,15 @@
-import { MetadataRoute } from 'next'
+import { NextResponse } from 'next/server'
 
-export function GET(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
+export async function GET() {
+  const robotsTxt = `
+User-agent: *
+Allow: /
+
+Sitemap: https://www.imagetools.co.kr/sitemap.xml
+`
+  return new NextResponse(robotsTxt, {
+    headers: {
+      'Content-Type': 'text/plain',
     },
-    sitemap: 'https://imagetools.co.kr/sitemap.xml',
-  }
+  })
 } 
