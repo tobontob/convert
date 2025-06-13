067d1fb1312c8a1b77f5d3e5529ac5a24da0c28a
tobontob <tobontob@naver.com>
2025-06-13 09:11:03 +0900

    up

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,60 +1,62 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://www.imagetools.co.kr'
+  
   return [
     {
-      url: 'https://www.imagetools.co.kr',
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
     },
     {
-      url: 'https://www.imagetools.co.kr/convert',
+      url: `${baseUrl}/convert`,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://www.imagetools.co.kr/compress',
+      url: `${baseUrl}/compress`,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://www.imagetools.co.kr/watermark',
+      url: `${baseUrl}/watermark`,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://www.imagetools.co.kr/rotate',
+      url: `${baseUrl}/rotate`,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://www.imagetools.co.kr/crop',
+      url: `${baseUrl}/crop`,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://www.imagetools.co.kr/resize',
+      url: `${baseUrl}/resize`,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
     {
-      url: 'https://www.imagetools.co.kr/privacy',
+      url: `${baseUrl}/privacy`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
-      url: 'https://www.imagetools.co.kr/terms',
+      url: `${baseUrl}/terms`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
-    }
+    },
   ]
 } 
